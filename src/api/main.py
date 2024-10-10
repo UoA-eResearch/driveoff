8fe80e195b3f50950af668e09f9b4044367dd98c
Andrew Wilson <andrew.wilson@auckland.ac.nz>
2024-10-11 12:41:03 +1300

    Add docstrings and address linter errors

--- src/api/main.py
+++ src/api/main.py
@@ -1,3 +1,5 @@
+"""Definition of endpoints/routers for the webserver."""
+
 import re
 from typing import Annotated, Any
 
@@ -11,6 +13,7 @@ RESEARCH_DRIVE_REGEX = re.compile(r"res[a-z]{3}[0-9]{9}-[a-zA-Z0-9-_]+")
 
 ENDPOINT_PREFIX = "/api/v1"
 
+
 def validate_resdrive_identifier(drive_id: str) -> str:
     """Check if the string is a valid Research Drive identifier."""
     if RESEARCH_DRIVE_REGEX.match(drive_id) is None:
@@ -22,17 +25,23 @@ def validate_resdrive_identifier(drive_id: str) -> str:
 ResearchDriveID = Annotated[str, AfterValidator(validate_resdrive_identifier)]
 
 
-
-# TODO: should this also accept the manifest?
 @app.post(ENDPOINT_PREFIX + "/resdriveinfo")
-def set_drive_info(drive_id: ResearchDriveID, ro_crate_metadata: dict[str, Any]) -> dict[str, str]:
+def set_drive_info(
+    drive_id: ResearchDriveID, ro_crate_metadata: dict[str, Any]
+) -> dict[str, str]:
+    """Submit initial RO-Crate metadata. NOTE: this may also need to accept the manifest data."""
+    _ = ro_crate_metadata
     return {
         "message": f"Received RO-Crate metadata for {drive_id}.",
     }
 
-# TODO: should this accept manifest updates?
+
 @app.put(ENDPOINT_PREFIX + "/resdriveinfo")
-def append_drive_info(drive_id: ResearchDriveID, ro_crate_metadata: dict[str, str]) -> dict[str, str]:
+def append_drive_info(
+    drive_id: ResearchDriveID, ro_crate_metadata: dict[str, str]
+) -> dict[str, str]:
+    """Submit additional RO-Crate metadata. NOTE: this may need to accept manifest deltas too."""
+    _ = ro_crate_metadata
     return {
         "message": f"Received additional RO-Crate metadata for {drive_id}.",
     }
@@ -40,6 +49,7 @@ def append_drive_info(drive_id: ResearchDriveID, ro_crate_metadata: dict[str, st
 
 @app.get(ENDPOINT_PREFIX + "/resdriveinfo")
 def get_drive_info(drive_id: ResearchDriveID) -> dict[str, str]:
+    """Retrieve information about the specified Research Drive."""
     return {
         "drive_id": drive_id,
         "ro_crate": "TODO: Make RO-Crate",
