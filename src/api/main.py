4fa5dbeacc40723931d9a109b303ba84a41d8131
andrew-uoa <142769327+andrew-uoa@users.noreply.github.com>
2024-10-23 15:25:56 +1300

    Create initial proof-of-concept webserver with stubs for requests (#1)
    * Add initial source file for API routing, with associated FastAPI dependency
    
    * Add stubs for the other methods needed
    
    * Add docstrings and address linter errors
    
    * Mark endpoint functions as async, to try and debug a hang issue
    
    * Disable test-running steps in the PR workflow until we have some tests
    
    * Add authentication to the webserver endpoints via API keys
    
    * Move the API keys file into the home folder
    
    * Update the gitignore
    
    * Add some test requests (for use with VS Code HTTP Client extension)

---
+++ src/api/main.py
@@ -0,0 +1,75 @@
+"""Definition of endpoints/routers for the webserver."""
+
+import re
+from typing import Annotated, Any
+
+from fastapi import FastAPI, Security
+from pydantic.functional_validators import AfterValidator
+
+from api.security import ApiKey, validate_api_key, validate_permissions
+
+app = FastAPI()
+
+
+RESEARCH_DRIVE_REGEX = re.compile(r"res[a-z]{3}[0-9]{9}-[a-zA-Z0-9-_]+")
+
+ENDPOINT_PREFIX = "/api/v1"
+
+
+def validate_resdrive_identifier(drive_id: str) -> str:
+    """Check if the string is a valid Research Drive identifier."""
+    if RESEARCH_DRIVE_REGEX.match(drive_id) is None:
+        raise ValueError(f"'{drive_id}' is not a valid Research Drive identifier.")
+
+    return drive_id
+
+
+ResearchDriveID = Annotated[str, AfterValidator(validate_resdrive_identifier)]
+
+
+@app.post(ENDPOINT_PREFIX + "/resdriveinfo")
+async def set_drive_info(
+    drive_id: ResearchDriveID,
+    ro_crate_metadata: dict[str, Any],
+    api_key: ApiKey = Security(validate_api_key),
+) -> dict[str, str]:
+    """Submit initial RO-Crate metadata. NOTE: this may also need to accept the manifest data."""
+
+    validate_permissions("POST", api_key)
+
+    _ = ro_crate_metadata
+    return {
+        "message": f"Received RO-Crate metadata for {drive_id}.",
+    }
+
+
+@app.put(ENDPOINT_PREFIX + "/resdriveinfo")
+async def append_drive_info(
+    drive_id: ResearchDriveID,
+    ro_crate_metadata: dict[str, str],
+    api_key: ApiKey = Security(validate_api_key),
+) -> dict[str, str]:
+    """Submit additional RO-Crate metadata. NOTE: this may need to accept manifest deltas too."""
+
+    validate_permissions("PUT", api_key)
+
+    _ = ro_crate_metadata
+    return {
+        "message": f"Received additional RO-Crate metadata for {drive_id}.",
+    }
+
+
+@app.get(ENDPOINT_PREFIX + "/resdriveinfo")
+async def get_drive_info(
+    drive_id: ResearchDriveID,
+    api_key: ApiKey = Security(validate_api_key),
+) -> dict[str, str]:
+    """Retrieve information about the specified Research Drive."""
+
+    validate_permissions("GET", api_key)
+
+    return {
+        "drive_id": drive_id,
+        "ro_crate": "TODO: Make RO-Crate",
+        "manifest": "TODO: Make manifest",
+    }
