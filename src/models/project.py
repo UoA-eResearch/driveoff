80436ae0563cbdc561518ef7054459f25cf28dfc
Noel <39939032+uoa-noel@users.noreply.github.com>
2024-11-14 20:00:56 +1300

    WIP working persistence

--- src/models/project.py
+++ src/models/project.py
@@ -1,15 +1,19 @@
 from datetime import datetime
-from typing import Optional
+from typing import TYPE_CHECKING, Optional
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 
-from models.person import InputPerson, Person
-from models.services import Services
+from models.person import InputPerson
+from models.services import InputServices, Services
+
+if TYPE_CHECKING:
+    from models.member import Member
 
 
-class InputCode(SQLModel):
+class Code(SQLModel, table=True):
     """Input model for project codes."""
 
+    id: Optional[int] = Field(primary_key=True)
     code: str
 
 
@@ -26,15 +30,26 @@ class BaseProject(SQLModel):
 class InputProject(BaseProject):
     """Input project model for data received from POST"""
 
+    id: Optional[int] = Field(default=None, primary_key=True)
     members: list[InputPerson]
-    codes: list[InputCode]
-    services: Services
+    codes: list[Code]
+    services: InputServices
 
 
-class Project(BaseProject):
+class ProjectCodeLink(SQLModel, table=True):
+    """Linking table between project and codes"""
+
+    code_id: int | None = Field(default=None, foreign_key="code.id", primary_key=True)
+    project_id: int | None = Field(
+        default=None, foreign_key="project.id", primary_key=True
+    )
+
+
+class Project(BaseProject, table=True):
     """Project model for data stored in database"""
 
     id: Optional[int] = Field(default=None, primary_key=True)
-    members: list[Person]
-    codes: list[str]
-    services: Services
+    services_id: int | None = Field(default=None, foreign_key="services.id")
+    codes: list[Code] = Relationship(link_model=ProjectCodeLink)
+    services: Services = Relationship()
+    members: list["Member"] = Relationship(back_populates="project")
--- src/models/services.py
+++ src/models/services.py
@@ -1,24 +1,44 @@
 from datetime import datetime
 from typing import Optional
 
-from sqlmodel import SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 
 
-class ResearchDriveService(SQLModel):
+class ResearchDriveService(SQLModel, table=True):
     """Object describing a research drive service."""
 
     allocated_gb: float
     date: datetime
     first_day: datetime
     free_gb: float
-    id: int
+    id: Optional[int] = Field(primary_key=True)
     last_day: Optional[datetime]
     name: str
     percentage_used: float
     used_gb: float
 
 
-class Services(SQLModel):
-    """Object describing relevant storage services."""
+class ResearchDriveServicesLink(SQLModel, table=True):
+    """Linking table between research drive service and a project's service."""
+
+    service_id: int | None = Field(
+        default=None, foreign_key="services.id", primary_key=True
+    )
+    research_drive_id: int | None = Field(
+        default=None, foreign_key="researchdriveservice.id", primary_key=True
+    )
+
+
+class InputServices(SQLModel):
+    """Input object describing relevant storage services."""
 
     research_drive: list[ResearchDriveService]
+
+
+class Services(SQLModel, table=True):
+    """Object describing relevant storage services."""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    research_drive: list[ResearchDriveService] = Relationship(
+        link_model=ResearchDriveServicesLink
+    )
