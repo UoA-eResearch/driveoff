2ee02cb77f3541d6c50ba62d33241914709f546a
Noel <noel.zeng@auckland.ac.nz>
2024-12-06 09:54:09 +1300

    WIP add submission API

---
+++ src/models/project_changes.py
@@ -0,0 +1,31 @@
+from typing import TYPE_CHECKING
+
+from sqlmodel import Field, SQLModel
+
+if TYPE_CHECKING:
+    from models.project import Project
+
+
+class ProjectChanges(SQLModel):
+    """A model for describing updates to a project."""
+
+    title: str | None = Field(default=None)
+    description: str | None = Field(default=None)
+
+    def apply_changes(self, to: "Project") -> bool:
+        """Update a project based on changed values in this instance.
+
+        Args:
+            to (Project): The project model to apply changes to.
+
+        Returns:
+            bool: If there were any changes applied.
+        """
+        applied_changes: bool = False
+        if self.title is not None and self.title != to.title:
+            to.title = self.title
+            applied_changes = True
+        if self.description is not None and self.description != to.description:
+            to.description = self.description
+            applied_changes = True
+        return applied_changes
--- src/models/submission.py
+++ src/models/submission.py
@@ -7,12 +7,37 @@ from pydantic import ConfigDict
 from sqlmodel import Field, Relationship, SQLModel
 
 from models.common import DataClassification
+from models.project_changes import ProjectChanges
 
 if TYPE_CHECKING:
     from models.services import ResearchDriveService
 
 
-class DriveOffboardSubmission(SQLModel, table=True):
+class BaseDriveOffboardSubmission(SQLModel):
+    """Model of drive offboarding submission with common fields."""
+
+    retention_period_years: int = Field(
+        schema_extra={"validation_alias": "retentionPeriodYears"}
+    )
+    retention_period_justification: str | None = Field(
+        default=None, schema_extra={"validation_alias": "retentionPeriodJustification"}
+    )
+    data_classification: DataClassification = Field(
+        schema_extra={"validation_alias": "dataClassification"}
+    )
+    is_completed: bool = Field(schema_extra={"validation_alias": "isCompleted"})
+
+
+class InputDriveOffboardSubmission(BaseDriveOffboardSubmission):
+    """Submission data model for the POST request."""
+
+    drive_name: str = Field(schema_extra={"validation_alias": "driveName"})
+    project_changes: ProjectChanges | None = Field(
+        default=None, schema_extra={"validation_alias": "projectChanges"}
+    )
+
+
+class DriveOffboardSubmission(BaseDriveOffboardSubmission, table=True):
     """Model that represents a user's submission in the drive
     offboarding process retrieved."""
 
@@ -21,10 +46,6 @@ class DriveOffboardSubmission(SQLModel, table=True):
     model_config = ConfigDict(str_strip_whitespace=True)  # type: ignore
 
     id: int | None = Field(default=None, primary_key=True)
-    retention_period_years: int
-    retention_period_justification: str | None = None
-    data_classification: DataClassification
-    is_completed: bool
     updated_time: datetime
     is_project_updated: bool
     drive_id: int | None = Field(default=None, foreign_key="researchdriveservice.id")
