1f2f6cbe0339fe1478dbfaa0962376a8040ab18c
JloveUOA <james.love@auckland.ac.nz>
2024-12-09 13:58:48 +1300

    convert factories to fixtures within conftest

--- tests/test_factories.py
+++ tests/test_factories.py
@@ -1,10 +1,20 @@
-import factories
+
+"functions and classes for testing the factories for populating classes"
+from factory.alchemy import SQLAlchemyModelFactory
 
 
-def test_factories_all() -> None:
-    factories.PersonFactory.build()
-    factories.CodeFactory.build()
-    factories.MemberFactory.build()
-    factories.ResearchDriveServiceFactory.build()
-    factories.DriveOffboardSubmissionFactory.build()
-    factories.ProjectFactory.build()
+def test_factories_all(# pylint: disable=too-many-arguments,too-many-positional-arguments
+    person_factory: SQLAlchemyModelFactory,
+    member_factory: SQLAlchemyModelFactory,
+    code_factory: SQLAlchemyModelFactory,
+    research_drive_service_factory: SQLAlchemyModelFactory,
+    drive_offboard_submission_factory: SQLAlchemyModelFactory,
+    project_factory: SQLAlchemyModelFactory
+    ) -> None:
+    "Test the basic functionality of all factories"
+    person_factory.build()
+    member_factory.build()
+    code_factory.build()
+    research_drive_service_factory.build()
+    drive_offboard_submission_factory.build()
+    project_factory.build()
