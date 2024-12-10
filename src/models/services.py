bfeb0370d11faa1265ea0070d6850f4532b70b25
LibbyLi667 <libbyjuan69@gmail.com>
2024-12-10 17:25:20 +1300

    remove unused module

--- src/models/services.py
+++ src/models/services.py
@@ -3,7 +3,6 @@
 from datetime import datetime
 from typing import TYPE_CHECKING, Optional
 
-from pydantic import BaseModel
 from sqlmodel import Field, Relationship, SQLModel
 
 from models.manifest import Manifest, ManifestDriveLink
